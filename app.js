06171d964b7728742982ec8bb8a7a567e010c8f7
fgbruna <fgbruna@uc.cl>
2018-12-13 16:05:11 -0300

    linting config and commands schema

--- app.js
+++ app.js
@@ -1,64 +1,80 @@
-/**
- * A bot that welcomes new guild members when they join
- */
-const gameinfo = require('./library.js').gameInfo;
-
 // Import the discord.js module
 const Discord = require('discord.js');
+
+const fs = require('fs');
 
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
-const discordKey = process.env.API_KEY
+client.commands = new Discord.Collection();
 
-const itadKey = process.env.ITAD_KEY
+const discordKey = process.env.API_KEY;
 
-const prefix = "!";
+const itadKey = process.env.ITAD_KEY;
+
+const prefix = process.env.PREFIX || '&';
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
+
 
 /**
  * The ready event is vital, it means that only _after_ this will your bot start reacting to information
  * received from Discord
  */
 client.on('ready', () => {
-    console.log('I am ready!');
+	console.log('I am ready on dev!');
 });
 
 // Create an event listener for new guild members
 client.on('guildMemberAdd', member => {
-    // Send the message to a designated channel on a server:
-    const channel = member.guild.channels.find(ch => ch.name === 'General');
-    // Do nothing if the channel wasn't found on this server
-    if (!channel) return;
-    // Send the message, mentioning the member
-    channel.send(`Welcome to the server, ${member}`);
+	// Send the message to a designated channel on a server:
+	const channel = member.guild.channels.find(ch => ch.name === 'General');
+	// Do nothing if the channel wasn't found on this server
+	if (!channel) return;
+	// Send the message, mentioning the member
+	channel.send(`Welcome to the server, ${member}`);
 });
 
 process.on('unhandledRejection', (reason, p) => {
-    console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
-    // application specific logging, throwing an error, or other logic here
+	console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+	// application specific logging, throwing an error, or other logic here
 });
 
 client.on('message', message => {
 
-    console.log(message.content);
+	console.log(message.content);
 
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
+	const args = message.content.slice(prefix.length).split(/ +/);
 
-    const command = args.shift().toLowerCase();
+	const command = args.shift().toLowerCase();
 
-    if (command === "info") {
-        gameinfo(itadKey, args[0]) // fetch info about game
-            .then(function (respuestas) {
-                // then send it to the channel
-                message.channel.send(JSON.stringify(respuestas['data'][args[0]]['urls']));
-            });
-    } else if (command === "ping") {
-        message.channel.send('Que te pasa maldyto' + message.author);
-    }
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	}
+ catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+
 });
 
 
 // Log our bot in using the token from https://discordapp.com/developers/applications/me
 client.login(discordKey);
+
+module.exports = {
+	itadKey,
+
+};
